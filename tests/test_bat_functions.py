62af4aed1e69e2d9fe5db7a9bddfa3e2fa3e3268
Vougiatzi <t8220020@aueb.gr>
2025-04-09 23:43:40 +0300

    Add parametrized test for signal_strength

--- tests/test_bat_functions.py
+++ tests/test_bat_functions.py
@@ -1,6 +1,7 @@
 import pytest
 import random
 from src.bat_functions import calculate_bat_power
+from src.bat_functions import signal_strength
 
 def test_calculate_bat_power():
     
@@ -9,3 +10,10 @@ def test_calculate_bat_power():
         bat_power = calculate_bat_power(level)
         assert  bat_power % 42 == 0
     
+@pytest.mark.parametrize("distance, expected", 
+                         [(0, 100), (5, 50), (10, 0), (12, 0)])
+
+def test_signal_strength(distance, expected):
+    
+    strength = signal_strength(distance)
+    assert strength == expected
