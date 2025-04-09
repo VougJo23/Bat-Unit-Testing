50a86ba2d7b2b6baa7e3e22f96ba448e1115b32a
Vougiatzi <t8220020@aueb.gr>
2025-04-10 01:17:46 +0300

    Add mock test for fetch_joker_info

--- tests/test_bat_functions.py
+++ tests/test_bat_functions.py
@@ -1,19 +1,21 @@
 import pytest
 import random
+import time
+
 from src.bat_functions import calculate_bat_power
 from src.bat_functions import signal_strength
 from src.bat_functions import get_bat_vehicle
+from src.bat_functions import fetch_joker_info
+
 
 def test_calculate_bat_power():
     
-
     for _ in range(10):
         level = random.randint(0, 100)
         bat_power = calculate_bat_power(level)
         assert  bat_power % 42 == 0
 
 
-    
 @pytest.mark.parametrize("distance, expected", 
                          [(0, 100), (5, 50), (10, 0), (12, 0)])
 
@@ -23,8 +25,7 @@ def test_signal_strength(distance, expected):
     assert strength == expected
 
 
-
-@pytest.fixture
+@pytest.fixture(scope="function")
 def vehicles():
     return {
         'Batmobile': {'speed': 200, 'armor': 80},
@@ -36,7 +37,18 @@ def test_get_bat_vehicle(vehicles):
     
     for name, specs in vehicles.items():
         assert get_bat_vehicle(name) == specs
-       
-    with pytest.raises(ValueError) as vehicle_not_found:
+
+    # Test Exception for unkown vehicle
+    with pytest.raises(ValueError):
         get_bat_vehicle('Batcraft')
 
+
+def test_fetch_joker_info(mocker):
+    
+    # Mock a fast response to skip the 1-second delay
+    mocker.patch('time.sleep', return_value=None)
+    
+    info = fetch_joker_info()
+    assert info == {'mischief_level': 100, 'location': 'unknown'}
+    
+    time.sleep.assert_called_once_with(1)
